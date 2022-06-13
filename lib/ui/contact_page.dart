5bc184199bb8f2f9ea29914b1b9437b9467e676e
Gilmar Oliveira Queiroz <enggilmarqueiroz@gmail.com>
2022-06-13 02:29:03 -0300

    Alteração dia 13/06/2022 às 2h20

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:agenda_de_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +15,12 @@ class ContactPage extends StatefulWidget {
 
 class _ContactPageState extends State<ContactPage> {
 
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  bool _userEdited = false;
+
   Contact _editedContact;
 
   @override
@@ -23,6 +31,10 @@ class _ContactPageState extends State<ContactPage> {
       _editedContact = Contact();
     }else {
       _editedContact = Contact.fromMap(widget.contact.toMap());
+
+      _nameController.text = _editedContact.name;
+      _emailController.text = _editedContact.email;
+      _phoneController.text = _editedContact.phone;
     }
   }
 
@@ -38,6 +50,55 @@ class _ContactPageState extends State<ContactPage> {
         onPressed: (){},
         child: Icon(Icons.save),
         backgroundColor: Colors.red,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                width: 140.0,
+                height: 140.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: _editedContact.img != null ?
+                      FileImage(File(_editedContact.img)) :
+                      AssetImage("images/person.png") as ImageProvider
+                  ),
+                ),
+              ),
+            ),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: "Nome"),
+              onChanged: (text){
+                _userEdited = true;
+                setState((){
+                  _editedContact.name = text;
+                });
+              },
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: "E-Mail"),
+              onChanged: (text){
+                _userEdited = true;
+                _editedContact.email = text;
+              },
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: _phoneController,
+            decoration: InputDecoration(labelText: "Phone"),
+            onChanged: (text){
+              _userEdited = true;
+              _editedContact.phone = text;
+              },
+              keyboardType: TextInputType.phone,
+            ),
+          ],
+        ),
       ),
     );
   }
