2cfc4ee19520208e0b145ab9b165128aceddb5fe
Gilmar Oliveira Queiroz <enggilmarqueiroz@gmail.com>
2022-06-18 21:23:38 -0300

    Alteração dia 13/06/2022 às 2h20

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -4,6 +4,8 @@ import 'package:agenda_de_contatos/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+enum OrderOptions {orderaz, orderza}
+
 class HomePage extends StatefulWidget {
   const HomePage({key}) : super(key: key);
 
@@ -31,6 +33,21 @@ class _HomePageState extends State<HomePage> {
         title: Text("Contatos"),
         backgroundColor: Colors.red,
         centerTitle: true,
+        actions: <Widget> [
+          PopupMenuButton<OrderOptions>(
+              itemBuilder: (context) => <PopupMenuEntry<OrderOptions>>[
+                const PopupMenuItem(
+                    child: Text("Ordenar de A-Z"),
+                    value: OrderOptions.orderaz,
+                ),
+                const PopupMenuItem<OrderOptions>(
+                    child: Text("Ordenar de Z-A"),
+                    value: OrderOptions.orderza,
+                ),
+              ],
+              onSelected: _orderList,
+          )
+        ],
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
@@ -179,4 +196,23 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void _orderList(OrderOptions result){
+    switch(result){
+      case OrderOptions.orderaz:
+        contacts.sort((a, b) {
+          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
+        break;
+      case OrderOptions.orderza:
+        contacts.sort((a, b) {
+          return b.name.toLowerCase().compareTo(a.name.toLowerCase());
+        });
+        break;
+    }
+    setState(() {
+
+    });
+  }
+
 }
+
